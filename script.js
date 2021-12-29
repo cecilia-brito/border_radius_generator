0007bb93f4fa2c5f6f41665490d403bbb65fd641
Cecilia Brito Santos <ceciliabritosantos@gmail.com>
2021-12-28 23:15:21 -0300

    adicionando script js e usando a propriedade onkeyup para executar a função

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-let borderBottomRight = document.getElementById('selector-border-bottom-right');
-let borderBottomLeft = document.getElementById('selector-border-bottom-left');
-let borderTopRight = document.getElementById('selector-border-top-right') ;
-let borderTopLeft = document.getElementById('selector-border-top-left');
-let formExample = document.getElementById('element-example');
-let borderRadius = 0;
 
-function calcBorderRadius(){
+// function calcBorderRadius(){
+//     borderTopLeft = inputTopLeft.value;
+//     borderTopRight = inputTopRight.value;
+//     borderBottomLeft = inputBottomLeft.value;
+//     borderBottomRight = inputBottomRight.value;
+// }
 
-}
 
-function changeBorder(){
-    formExample.style.borderBottomLeftRadius = borderRadius + '%';
-    formExample.style.borderBottomRightRadius = borderRadius + '%';
-    formExample.style.borderTopLeftRadius = borderRadius + '%';
-    formExample.style.borderTopRightRadius = borderRadius + '%';
-}
 
